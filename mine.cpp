afb9d5e104b215109a8292956509b925cc235ead
unknownblueguy6 <unknownblueguy6@gmail.com>
2019-02-11 17:32:54 +0530

    Add movement

--- mine.cpp
+++ mine.cpp
@@ -1,39 +1,108 @@
 #include <iostream>
+#include <unistd.h>
+#include <termios.h>
 
-using namespace std;
 
-string red_bg = "\033[1;41m";
-string reset = "\033[0m";
+typedef char KEY;
+
+enum KEYS{
+	K_ESC = '\e',
+	K_BRAC_OPEN = '[',
+	K_UP = 'A',
+	K_DOWN = 'B',
+	K_LEFT = 'D',
+	K_RIGHT = 'C'
+}; 
+
+char getch() {
+	char buf = 0;
+	struct termios old = {0};
+	if (tcgetattr(0, &old) < 0)
+		perror("tcsetattr()");
+	old.c_lflag &= ~ICANON;
+	old.c_lflag &= ~ECHO;
+	old.c_cc[VMIN] = 1;
+	old.c_cc[VTIME] = 0;
+	if (tcsetattr(0, TCSANOW, &old) < 0)
+		perror("tcsetattr ICANON");
+	if (read(0, &buf, 1) < 0)
+		perror("read()");
+	old.c_lflag |= ICANON;
+	old.c_lflag |= ECHO;
+	if (tcsetattr(0, TCSADRAIN, &old) < 0)
+		perror("tcsetattr ~ICANON");
+	return (buf);
+}
+
+KEY getKey(){
+	char c = getch();
+	c = (c == K_ESC) ? getch() : c;
+	c = (c == K_BRAC_OPEN) ? getch() : c;
+	return c;
+}
+
+
+std::string red_bg = "\033[1;41m";
+std::string reset = "\033[0m";
+std::string endl = "\n";
+
+int l, b;
 
 void drawGrid(int l, int b, int x, int y){
-	cout << "┌" ;
-	for(int i = 0; i < l - 1; ++i) cout << "───┬";
-	cout << "───┐";
-	cout << endl;
+    std::cout << endl;
+	std::cout << "┌" ;
+	for(int i = 0; i < l - 1; ++i) std::cout << "───┬";
+	std::cout << "───┐";
+	std::cout << endl;
 	for(int j = 0; j < b; ++j){
-        cout << "│";
+        std::cout << "│";
 		for(int k = 0; k < l; ++k){
-            cout << " ";
-            if(j == y && k == x) cout << red_bg;
-            cout << " " << reset << " │";
+            std::cout << " ";
+            if(j == y && k == x) std::cout << red_bg;
+            std::cout << " " << reset << " │";
 		}
         if(j != b-1) {
-            cout << endl << "├";
-            for(int k = 0; k < l-1; ++k) cout << "───┼";
-            cout << "───┤";
+            std::cout << endl << "├";
+            for(int k = 0; k < l-1; ++k) std::cout << "───┼";
+            std::cout << "───┤";
         }
-        cout << endl;
+        std::cout << endl;
 	}
-    cout << "└";
-    for(int i = 0; i < l - 1; ++i) cout << "───┴";
-    cout << "───┘";
-	
+    std::cout << "└";
+    for(int i = 0; i < l - 1; ++i) std::cout << "───┴";
+    std::cout << "───┘";
+	std::cout << endl;
+}
+
+void getPos(int &x, int &y){
+    KEY k = getKey();
+    switch(k){
+        case K_UP : 
+            if(y) --y;
+            return;
+        case K_DOWN : 
+            if(y != b-1) ++y;
+            return;
+        case K_LEFT:
+            if(x) --x;
+            return;
+        case K_RIGHT:
+            if(x != l-1) ++x;
+            return;  
+    }
 }
 
 int main(){
-    int l, b, x, y;
-    cin >> l >> b >> x >> y;
-	drawGrid(l, b, x, y);
-    cout << endl;
+    int x, y;
+    std::cin >> l >> b;
+    x = l/2;
+    y = b/2;
+
+    while(true){
+        drawGrid(l, b, x, y);
+        getPos(x, y);
+        system("clear");
+    }
+    std::cout << endl;
 	return 0;
 }
