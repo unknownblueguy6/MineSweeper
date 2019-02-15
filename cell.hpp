2a5600e2a42f69307f1e32eefba52b87fd12381c
unknownblueguy6 <unknownblueguy6@gmail.com>
2019-02-15 23:15:38 +0530

    Add blue_fg to COLOUR

--- cell.hpp
+++ cell.hpp
@@ -16,6 +16,7 @@ using COLOUR = const std::string;
 COLOUR red_fg = "\033[1;31m";
 COLOUR green_fg = "\033[1;32m";
 COLOUR yellow_fg = "\033[1;33m";
+COLOUR blue_fg = "\033[1;34m";
 COLOUR magenta_fg = "\033[1;35m";
 COLOUR cyan_fg = "\033[1;36m";
 COLOUR white_fg = "\033[1;37m";
