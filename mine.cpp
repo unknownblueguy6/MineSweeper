4f7be0c75cc5025b96a27a5a31a3557a4ef363d9
unknownblueguy6 <unknownblueguy6@gmail.com>
2019-02-16 13:08:04 +0530

    Add a banner

--- mine.cpp
+++ mine.cpp
@@ -1,12 +1,14 @@
 #include "field.hpp"
+#include "display.hpp"
 
 int main(){
     while(game == RUNNING){
+        dispBanner();
         field.drawField();
         field.getMove();
         system("clear");
     }
-    
+    dispBanner();
     field.drawField();
     if(game == VICTORY) std::cout << green_fg << endl << "You won!" << reset;
     else std::cout << red_fg << endl << "You lost!" << reset;
